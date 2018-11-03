71b216d52dc00cdc92264850157ff318f3b4e295
Kuisma Joentakanen <kuisma.joentakanen@gmail.com>
2018-11-03 18:38:25 +0200

    Create separate CSS file for ParticipansTable component, do some mobile responsive magic on the table.

--- src/ParticipantsTable.js
+++ src/ParticipantsTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ParticipantsTable.css';
 
 // TODO: pass data to table
 class ParticipantsTable extends Component {
@@ -6,18 +7,20 @@ class ParticipantsTable extends Component {
       return (
         <table className="participants-table">
         <tbody>
-        <tr>
+        <tr className="controls">
             <th>Name</th>
             <th>E-mail address</th>
             <th>Phone number</th>
+            <th></th>
         </tr>
 
         {this.props.data.map(function(item, index) {
           return (
             <tr key={item.user_id}>
-              <td>{item.full_name}</td>
-              <td>{item.email}</td>
-              <td>{item.phone}</td>
+              <td className="full_name"><label className="mobile">Name</label>{item.full_name}</td>
+              <td className="email"><label className="mobile">E-mail address</label>{item.email}</td>
+              <td className="phone"><label className="mobile">Phone number</label>{item.phone}</td>
+              <td className="options"><span className="pencil"></span><span className="trashcan"></span></td>
             </tr>
           )
         })}
