fed45dfcd0131a53fd2a22c1f297b5efafd301d9
Kuisma Joentakanen <kuisma.joentakanen@gmail.com>
2018-11-04 00:08:37 +0200

    Add events to form

--- src/AddParticipantForm.js
+++ src/AddParticipantForm.js
@@ -4,19 +4,30 @@ import './AddParticipantForm.css';
 class AddParticipantForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            full_name: "",
+            email: "",
+            phone: ""
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
     
-        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        console.log(event.target.value);
-        this.setState({value: event.target.value});
-    }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        console.log(this.state);
+        alert('A name was submitted: ' + this.state);
         event.preventDefault();
     }
 
@@ -24,9 +35,9 @@ class AddParticipantForm extends Component {
         return (
             <div className="add-participant-form">
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" placeholder="Full name" value={this.state.value.full_name} onChange={this.handleChange} />
-                    <input type="text" placeholder="E-mail address" value={this.state.value.email} onChange={this.handleChange} />
-                    <input type="text" placeholder="Phone number" value={this.state.value.phone} onChange={this.handleChange} />
+                    <input type="text" placeholder="Full name" name="full_name" value={this.state.full_name} onChange={this.handleInputChange} />
+                    <input type="text" placeholder="E-mail address" name="email" value={this.state.email} onChange={this.handleInputChange} />
+                    <input type="text" placeholder="Phone number" name="phone" value={this.state.phone} onChange={this.handleInputChange} />
                     <input type="submit" value="Add new" />
                 </form>
             </div>
